d9d33ae344a5de5f7abc76466d69aad44b9d59f8
cherryid1911 <cherryid@post.bgu.ac.il>
2025-06-21 19:52:58 +0300

    Q5.2

--- ex4.pl
+++ ex4.pl
@@ -39,26 +39,44 @@ book(the_lord_of_the_rings, t, f, s(s(s(s(s(s(zero))))))).
 
 % You can add more facts.
 
+max_list([], false).
+max_list([X], X).
+max_list([H|T], Max) :-
+    max_list(T, MaxTail),
+    max_num(H, MaxTail, Max).
 
-% Signature: max_list(Lst, Max)/2
-% Purpose: true if Max is the maximum church number in Lst, false if Lst is emoty.
+max_num(false, B, B).
+max_num(A, false, A).
+max_num(A, B, A) :- greater_than(A, B), !.
+max_num(A, B, B).
 
-
-
-
-
-
+greater_than(s(_), zero).
+greater_than(s(A), s(B)) :- greater_than(A, B).
 
 
 % Signature: author_of_genre(GenreName, AuthorName)/2
 % Purpose: true if an author by the name AuthorName has written a book belonging to the genre named GenreName.
-
-
-
-
-
-
+author_of_genre(GenreName, AuthorName) :-
+    genre(GenreTerm, GenreName),
+    author(AuthorTerm, AuthorName),
+    findall(BookName, book(BookName, AuthorTerm, GenreTerm, _), BookList),
+    BookList \= [].
 
 
 % Signature: longest_book(AuthorName, BookName)/2
 % Purpose: true if the longest book that an author by the name AuthorName has written is titled BookName.
+
+longest_book(AuthorName, BookName) :-
+    author(AuthorTerm, AuthorName),
+    findall((Length, Title), book(Title, AuthorTerm, _, Length), List),
+    max_book(List, (MaxLength, BookName)),
+    MaxLength \= false.
+
+max_book([], (false, false)).
+max_book([(Length, Title)], (Length, Title)).
+max_book([(L1, T1) | Rest], (MaxLength, MaxTitle)) :-
+    max_book(Rest, (LRest, TRest)),
+    max_num(L1, LRest, LMax),
+    ( LMax = L1 -> MaxLength = L1, MaxTitle = T1
+    ; MaxLength = LRest, MaxTitle = TRest
+    ).
